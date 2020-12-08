bb06f3b50bec551c274c769a30d97a3a18e8ddf5
Jhon J Diaz <diazj691@gmail.com>
2020-12-08 18:14:54 -0500

    agragando clase reporteador

---
+++ App/Reporteador.cs
@@ -0,0 +1,29 @@
+﻿using CoreEscuela.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEscuela.App
+{
+    class Reporteador
+    {
+        private Dictionary<LlaveDiccionario, IEnumerable<EscuelaBase>> diccionario;
+
+        public Reporteador(Dictionary<LlaveDiccionario, IEnumerable<EscuelaBase>> diccionarioReporte)
+        {
+            diccionario = diccionarioReporte ?? throw new ArgumentException(nameof(diccionarioReporte));
+        }
+
+        public IEnumerable<Asignatura> GetListAsignaturas()
+        {
+            if (diccionario.TryGetValue(LlaveDiccionario.Asignatura, out IEnumerable<EscuelaBase> dic))
+            {
+                return dic.Cast<Asignatura>();
+            }
+
+            return null;
+        }
+    }
+}
