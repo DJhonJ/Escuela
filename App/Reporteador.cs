6eb07fff688cc9fafb6cef8518a0726867f33346
Jhon J Diaz <diazj691@gmail.com>
2020-12-08 21:16:09 -0500

    reporte evaluaciones por asignatura

--- App/Reporteador.cs
+++ App/Reporteador.cs
@@ -20,7 +20,22 @@ namespace CoreEscuela.App
         {
             if (diccionario.TryGetValue(LlaveDiccionario.Asignatura, out IEnumerable<EscuelaBase> dic))
             {
-                return dic.Cast<Asignatura>();
+                List<Asignatura> asignaturas = new List<Asignatura>();
+
+                dic.Cast<Asignatura>().ToList().ForEach(asignatura =>
+                {
+                    if (asignaturas.Count > 0)
+                    {
+                        var response = asignaturas.ToList().FindAll((asig) => asignatura.Nombre == asig.Nombre).ToList();
+                        if (response.Count == 0) asignaturas.Add(asignatura);
+                    }
+                    else
+                    {
+                        asignaturas.Add(asignatura);
+                    }
+                });
+
+                return asignaturas;
             }
 
             return null;
@@ -32,7 +47,19 @@ namespace CoreEscuela.App
 
             return (from Asignatura a in asignaturas
                     from Evaluacion ev in a.Evaluciones
-                    select ev.Nombre).Distinct();
+                    select ev.Nombre);
+        }
+
+        public Dictionary<string, IEnumerable<Evaluacion>> GetEvaluacionPorAsignatura()
+        {
+            var diccionario = new Dictionary<string, IEnumerable<Evaluacion>>();
+            var asignaturas = GetListAsignaturas();
+
+            asignaturas.ToList().ForEach(asignaruta => {
+                diccionario.Add(asignaruta.Nombre, asignaruta.Evaluciones);
+            });
+
+            return diccionario;
         }
     }
 }
